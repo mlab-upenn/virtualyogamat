e823e73b6ad0a4a2fd0f2a100747e40dcceaba60
Juliusgeo <juliusgeo@gmail.com>
2016-08-15 14:26:53 -0400

    used sally branch code, updated ui, added video and pose system

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,32 +5,33 @@ import { Pose } from '../api/pose.js';
 import './body.html';
 
 Template.body.events({
-  'submit .new-task'(event) {
-    event.preventDefault();
- 
-    // Get value from form element
-    const target = event.target;
-    const text = target.text.value;
+  'click [data-action=\'task/save\']': function(e){
+    e.preventDefault();
+    console.log("save");
     var result = [];
+    var player = document.getElementById('player');
     Mat.find({}).forEach(function(u) { result.push(u.lit) });
     // Insert a task into the collection
-    Pose.insert({time: text, pose: result});
- 
-    // Clear form
-    target.text.value = '';
+    Pose.insert({pose: result, time: player.currentTime, desc: document.getElementById("yogaPoseDescription").value, name: document.getElementById("poseName").value});
+
   },
+  'click [data-action=\'task/loadvid\']': function(e){
+    console.log("play")
+    var player = document.getElementById('player');
+    var source = document.createElement('source');
+    if (player.hasChildNodes()) {player.removeChild(player.childNodes[0]);}
+    source.setAttribute('src', document.getElementById("vidURL").value);
+    //source.setAttribute('src', 'http://www.w3schools.com/html/mov_bbb.mp4');
+    player.setAttribute('style','display: block;')
+    player.appendChild(source);
+    player.play();
+  }
+
 });
 Template.body.helpers({
   mat: function() {
     return Mat.find({},{lit: 1}).fetch();
   },
-  litcolor: function(){
-    var result = [];
-    Mat.find({_id: this._id},{lit:1}).forEach(function(u) { result.push(u.lit) });
-    if(result[0]==="true"){return "rgb(220,220,220)";};
-    if(result[0]==="false"){return "#000000";};
-
-  }
 });
 Template.section.events({
   'click .matsection': function(e){
@@ -46,11 +47,24 @@ Template.section.events({
     Mat.update({_id: this._id},{$set: {lit: (!curval).toString()}});
 
   },
-  
-});
+  });
 Handlebars.registerHelper('litcolor', function() {
     var result = [];
     Mat.find({_id: this._id},{lit:1}).forEach(function(u) { result.push(u.lit) });
     if(result[0]==="true"){return "rgb(220,220,220)";};
     if(result[0]==="false"){return "#000000";};
+});
+Template.section.helpers({
+    styles(){
+        var result = [];
+    Mat.find({_id: this._id},{styles: 1, lit: 0}).forEach(function(u) { result.push(u.styles) });
+    return result;
+    },
+    //imgsrc(){
+    //    var result = [];
+    //    Mat.find({_id: this._id},{imgsrc:1, styles: 0, lit: 0}).forEach(function(u) { result.push(u.styles) });
+    //    return result;
+    //  }
+    //}
+
 });
