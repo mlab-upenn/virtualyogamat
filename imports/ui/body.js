60ca36519f15de81d8952e1c0722b9316b8e01f7
Juliusgeo <juliusgeo@gmail.com>
2016-09-20 22:19:25 -0400

    added boxes back in

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,6 +2,9 @@ import { Template } from 'meteor/templating';
 import { Mat } from '../api/mat.js';
 import { Pose } from '../api/pose.js';
 
+//for(i=2; i<18; i++){
+//  Mat.insert({imgsrc: "matLayer"+i+".svg"});
+//}
 import './body.html';
 
 Template.body.events({
@@ -12,15 +15,16 @@ Template.body.events({
     var player = document.getElementById('player');
     Mat.find({}).forEach(function(u) { result.push(u.lit) });
     // Insert a task into the collection
-    Pose.insert({pose: result, time: player.currentTime, desc: document.getElementById("yogaPoseDescription").value, name: document.getElementById("poseName").value});
+    Pose.insert({vidfile: path.substr(path.lastIndexOf('/')+1), pose: result, time: player.currentTime, desc: document.getElementById("yogaPoseDescription").value, name: document.getElementById("poseName").value});
 
   },
   'click [data-action=\'task/loadvid\']': function(e){
     console.log("play")
     var player = document.getElementById('player');
     var source = document.createElement('source');
+    path = document.getElementById("vidURL").value;
     if (player.hasChildNodes()) {player.removeChild(player.childNodes[0]);}
-    source.setAttribute('src', document.getElementById("vidURL").value);
+    source.setAttribute('src', path);
     //source.setAttribute('src', 'http://www.w3schools.com/html/mov_bbb.mp4');
     player.setAttribute('style','display: block;')
     player.appendChild(source);
@@ -32,6 +36,7 @@ Template.body.helpers({
   mat: function() {
     return Mat.find({},{lit: 1}).fetch();
   },
+
 });
 Template.section.events({
   'click .matsection': function(e){
@@ -54,17 +59,21 @@ Handlebars.registerHelper('litcolor', function() {
     if(result[0]==="true"){return "rgb(220,220,220)";};
     if(result[0]==="false"){return "#000000";};
 });
+Handlebars.registerHelper('imgsrc', function() {
+
+
+});
+
 Template.section.helpers({
     styles(){
         var result = [];
     Mat.find({_id: this._id},{styles: 1, lit: 0}).forEach(function(u) { result.push(u.styles) });
     return result;
     },
-    //imgsrc(){
-    //    var result = [];
-    //    Mat.find({_id: this._id},{imgsrc:1, styles: 0, lit: 0}).forEach(function(u) { result.push(u.styles) });
-    //    return result;
-    //  }
-    //}
+    imgsrc(){
+        var result = [];
+        Mat.find({_id: this._id},{imgsrc:1}).forEach(function(u) { result.push(u.imgsrc) });
+        return result;
+      }
 
 });
